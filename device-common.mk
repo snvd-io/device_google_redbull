2f0326242c13ff367e63e8251c24b37944480556
Daniel Micay <danielmicay@gmail.com>
2020-10-14 19:16:12 -0400

    disable system_other odex

--- device-common.mk
+++ device-common.mk
@@ -145,7 +145,6 @@ endif
 # A/B support
 PRODUCT_PACKAGES += \
     otapreopt_script \
-    cppreopts.sh \
     update_engine \
     update_verifier
 
@@ -165,9 +164,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PACKAGES += \
     bootctrl.lito \
     bootctrl.lito.recovery
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.cp_system_other_odex=1
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
